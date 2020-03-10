3b3742a492705b5be7d63c2f3f6e2fdcf9b5b7ed
Pawel <pgrzeszczak@ethernet.pl>
2020-03-10 06:01:29 +0100

    Send emails and use Thymeleaf examples material 32.3 & Fix 32.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -18,14 +21,29 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Board");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("preview", preview(message));
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend-extended/");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("company_config", adminConfig.getCompanyName());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    private String preview(String message) {
+        String[] words = message.split(" ");
+        return words[0] + " " + words[1] + " " + words[2] + "...";
     }
 
 }
